f6b4948ab5e9c495dfba66d9aa713c7c4a96604a
Charlie Plate <charlie@graphiteconnect.com>
2024-06-12 16:11:31 -0400

    Protobuf: Fix Protobuf "maestro.Parser" implementation

--- pb/proto_test.go
+++ pb/proto_test.go
@@ -3,6 +3,7 @@ package pb_test
 import (
 	"testing"
 
+	"github.com/charlieplate/maestro"
 	"github.com/charlieplate/maestro/pb"
 	"github.com/google/go-cmp/cmp"
 	"github.com/stretchr/testify/require"
@@ -13,22 +14,18 @@ import (
 
 type testMsg struct {
 	Content proto.Message
-	ConnID  string
 	Version string
 }
 
-func unsafeUnmarshalMsg(tm testMsg, content proto.Message) []byte {
+func mustUnmarshalMessage(tm testMsg, content proto.Message) []byte {
 	a := &anypb.Any{}
 	err := a.MarshalFrom(content)
 	if err != nil {
 		panic(err)
 	}
 
-	d := []byte{0x00, 0x00, 0x00, 0x01}
-
 	w := &pb.Message{
 		Content:      a,
-		ConnId:       tm.ConnID,
 		ProtoVersion: tm.Version,
 	}
 
@@ -37,10 +34,25 @@ func unsafeUnmarshalMsg(tm testMsg, content proto.Message) []byte {
 		panic(err)
 	}
 
-	d = append(d, pb.IntToBytes(len(data), 4)...)
-	d = append(d, data...)
+	return data
+}
 
-	return d
+func TestNewProtobufParser(t *testing.T) {
+	tests := []struct {
+		want *pb.ProtobufParser
+		name string
+	}{
+		{
+			name: "Test NewProtobufParser",
+			want: &pb.ProtobufParser{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			require.Equal(t, tt.want, pb.NewProtobufParser())
+			require.Implements(t, (*maestro.Parser)(nil), tt.want)
+		})
+	}
 }
 
 func TestProtobuf_ParseIncoming(t *testing.T) {
@@ -49,44 +61,39 @@ func TestProtobuf_ParseIncoming(t *testing.T) {
 		ExpectedContentType any
 		ExpectedError       error
 		Name                string
-		ExpectedConnID      string
 		Incoming            []byte
 	}{
 		{
 			Name: "Valid Input",
-			Incoming: unsafeUnmarshalMsg(testMsg{
+			Incoming: mustUnmarshalMessage(testMsg{
 				Version: "3.0.0",
-				ConnID:  "123",
 			}, &pb.Subscribe{
 				Queue: "test123",
 			}),
 			ExpectedContent: &pb.Subscribe{
 				Queue: "test123",
 			},
-			ExpectedConnID: "123",
-			ExpectedError:  nil,
+			ExpectedError: nil,
 		},
 		{
 			Name: "Invalid Version",
-			Incoming: unsafeUnmarshalMsg(testMsg{
+			Incoming: mustUnmarshalMessage(testMsg{
 				Version: "2.0.0",
 			}, &pb.Subscribe{}),
 			ExpectedContent: nil,
-			ExpectedConnID:  "",
 			ExpectedError:   pb.ErrInvalidProtoVersion,
 		},
 	}
 
 	for _, tc := range testCases {
 		t.Run(tc.Name, func(t *testing.T) {
-			pbc := pb.ProtobufDecoder{}
-			msg, err := pbc.ParseIncoming(tc.Incoming)
+			pbc := pb.ProtobufParser{}
+			msg, err := pbc.Parse(tc.Incoming)
 			if tc.ExpectedError != nil {
 				require.Error(t, err, "Error expected")
 			} else {
 				require.NoError(t, err, "Error not expected")
-				require.NotNil(t, msg, "ConnID IS nil")
-				require.Equal(t, tc.ExpectedConnID, msg.ConnID, "ConnID mismatch")
+				require.Zero(t, msg.ConnID, "ConnID should be nil")
 				require.True(t, cmp.Equal(tc.ExpectedContent, msg.Content, protocmp.Transform()), cmp.Diff(tc.ExpectedContent, msg.Content, protocmp.Transform()))
 			}
 		})
